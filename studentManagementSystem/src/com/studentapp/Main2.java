b90dd6178c5540b4442e25943a1c5828ab79a9b5
Chira <Chira@DESKTOP-8PUJGIU>
2026-02-01 19:56:54 +0530

    add exception custom message

--- studentManagementSystem/src/com/studentapp/Main2.java
+++ studentManagementSystem/src/com/studentapp/Main2.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main2 {
+	
+	private static List <Student> studentList;
 
 	public static void main(String[] args) {
 		System.out.println("*************** Student Management System ***************");
 		System.out.println("************* Welcome *************");
 		
-		List <Student> studentList  = new ArrayList<Student>();
+		studentList  = new ArrayList<Student>();
 		
 		Student s1; //reference variable  which stores the unique Identification or Hash code of the object created in Heap memory
 		s1 = new Student("Ajay", 22, "S-1");  // variable inside the method are called as local variable and store in stack memory
@@ -39,9 +41,23 @@ public class Main2 {
 		studentList.add(s1);
 		studentList.add(s2);
 		studentList.add(s3);
+		
+		Student result = findStudentById("S-23");
+		System.out.println("Result " + result);
 
 	}
 	
-	public void findStudentById(String )
+	public static Student findStudentById(String studentId ) {
+	Student result = null;	
+	
+	try {
+		result =	studentList.stream().filter(x -> x.getStudentId().equalsIgnoreCase(studentId)).findFirst().orElseThrow(()-> new RuntimeException("No Data Found !!!!"));
+	}
+	catch(RuntimeException e) {
+		System.err.println("Student with ID " + studentId+ " not found !!");
+	}
+	return result;
+		
+	}
 
 }
